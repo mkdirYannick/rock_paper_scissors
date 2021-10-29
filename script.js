c6d14d667ff63853728448500ef1c353729953af
mkdirYannick <yannick.pell@gmail.com>
2021-10-29 12:22:23 +0200

    modified script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function computerPlay () {
     return computerSelection = randomItem;
 }
 
-computerPlay();
+//computerPlay();
 
 function userInput() {
     let userChoice = prompt('Please Enter Rock, Paper or Scissors', 'Rock').toLowerCase();
@@ -18,8 +18,7 @@ function userInput() {
 
 //userInput();
 
-console.log('You played ' + playerSelection)
-console.log('The computer played ' + computerSelection)
+
 
 //Tout ce qui est au dessus fonctionne : La fonction computerPlay génère aléatoirement un choix 
 //et retourne ce choix dans la variable computerSelection.
@@ -50,8 +49,51 @@ function playRound (playerSelection, computerSelection) {
     }       
 }
 
-console.log(playRound(playerSelection, computerSelection));
+//console.log('You played ' + playerSelection)
+//console.log('The computer played ' + computerSelection)
+//console.log(playRound(playerSelection, computerSelection));
 
 // Everything above works as intended. The computer generate a random choice between rock, paper and scissors,
 // and the player is prompt to choose as well. Then, the function playRound test each case to determine wether it 
 // is a draw first, then win or lose, and return the appropriate statement. I've tested every single case possible and they all worked.
+
+let userScore = 0;
+let computerScore = 0;
+
+function game() {
+    computerPlay();
+    userInput();
+    playRound();
+    let result = playRound(playerSelection, computerSelection);
+    console.log('You played ' + playerSelection)
+    console.log('The computer played ' + computerSelection)
+    console.log(result);
+    if (result.includes("win")) {
+        return userScore += 1;
+    } else if (result.includes("lose")) {
+        return computerScore += 1;
+    }
+}
+
+function match() {
+    let numberOfRound = parseInt(prompt('Enter the number of round:', '3'));
+    if (numberOfRound < 0 || numberOfRound > 100) {
+        console.log('Please enter a valid number.');
+    } else { 
+        for (var i = 0; i < numberOfRound; i++) {
+        game();
+        }
+        console.log('Your score: ' + userScore);
+        console.log('Computer score: ' + computerScore);
+        if (userScore > computerScore) {
+            console.log('You win the game! Go humans!');
+        } else if (userScore < computerScore) {
+            console.log("Oh no! You lost to the machine :/");
+        } else {
+            console.log('The final result is a draw!');
+        }
+    }    
+}
+
+match();
+
